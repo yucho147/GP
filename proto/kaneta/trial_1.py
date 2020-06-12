fd80ac20681f87ccb370304b0fbda31affdcee88
Kaneta Yuya <yucho147@gmail.com>
2020-06-12 12:30:44 +0900

    これでmainはすっきりしたかな

--- proto/kaneta/trial_1.py
+++ proto/kaneta/trial_1.py
@@ -5,15 +5,11 @@ import torch
 
 
 class GPModel(gpytorch.models.ExactGP):
-    """Documentation for GPModel
-
-    """
     def __init__(self, train_x, train_y, likelihood,
                  lengthscale_prior=None, outputscale_prior=None):
         super(GPModel, self).__init__(train_x,
                                       train_y,
-                                      likelihood,
-                                      )
+                                      likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
         self.covar_module = gpytorch.kernels.ScaleKernel(
             gpytorch.kernels.RBFKernel(lengthscale_prior=lengthscale_prior),
@@ -25,24 +21,53 @@ class GPModel(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-class Trainer(object):
-    """Documentation for Trainer
+class Run(object):
+    def __init__(self,
+                 gpr='ExactGP',
+                 l_prior=gpytorch.priors.NormalPrior(loc=torch.tensor(1.), scale=torch.tensor(10.)),
+                 s_prior=gpytorch.priors.NormalPrior(loc=torch.tensor(1.), scale=torch.tensor(10.)),
+                 likelihood='GaussianLikelihood',
+                 optimizer='RMSprop',
+                 mll='ExactMarginalLogLikelihood'):
+        self.l_prior = l_prior
+        self.s_prior = s_prior
+        self._likelihood = likelihood
+        self._set_likelihood()
+        self._gpr = gpr
+        self._optimizer = optimizer
+        self._mll = mll
 
-    """
-    def __init__(self, gpr, likelihood, optimizer, mll):
-        self.gpr = gpr
-        self.likelihood = likelihood
-        self.optimizer = optimizer
-        self.mll = mll
+    def _set_likelihood(self):
+        if self._likelihood == 'GaussianLikelihood':
+            self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        else:
+            raise ValueError
+
+    def set_model(self, train_x, train_y):
+        if self._gpr == 'ExactGP':
+            # ここで上記モデルのインスタンスを立てる
+            self.model = GPModel(train_x, train_y, self.likelihood,
+                                 self.l_prior, self.s_prior)
+            if self._mll == 'ExactMarginalLogLikelihood':
+                self.mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
+                if self._optimizer == 'RMSprop':
+                    self.optimizer = torch.optim.RMSprop(params=self.model.parameters(),
+                                                         lr=1e-2)
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+        else:
+            raise ValueError
 
     def update_hyperparameter(self, epochs):
-        self.gpr.train()
+        self.model.train()
         self.likelihood.train()
         for epoch in range(epochs):
             self.optimizer.zero_grad()
-            output = self.gpr(self.gpr.train_inputs[0])
+            output = self.model(self.model.train_inputs[0])
 
-            loss = - self.mll(output, self.gpr.train_targets)
+            loss = - self.mll(output, self.model.train_targets)
             loss.backward()
             self.optimizer.step()
 
@@ -56,26 +81,17 @@ def main():
     train_inputs = torch.linspace(0, 1, 10).reshape(10, 1)
     train_targets = torch.sin(2*np.pi*train_inputs).reshape(10) + 0.3*torch.randn(10)
 
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    l_prior = gpytorch.priors.NormalPrior(loc=torch.tensor(1.),
-                                          scale=torch.tensor(10.))
-    s_prior = gpytorch.priors.NormalPrior(loc=torch.tensor(1.),
-                                          scale=torch.tensor(10.))
-    gpr = GPModel(train_inputs, train_targets, likelihood,
-                  lengthscale_prior=l_prior, outputscale_prior=s_prior)
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, gpr)
-
-    optimizer = torch.optim.RMSprop(params=gpr.parameters(),
-                                    lr=1e-2)
-    trainer = Trainer(gpr, likelihood, optimizer, mll)
-    trainer.update_hyperparameter(2000)
+    run = Run()
+    run.set_model(train_inputs, train_targets)
+    run.update_hyperparameter(2000)
 
     test_inputs = torch.linspace(-0.2, 1.2, 100)
 
-    gpr.eval()
-    likelihood.eval()
+    # Runクラスではupdate_hyperparameterしかまだ作っていないのでpredictは下記のように直打ちしているっす。
+    run.model.eval()
+    run.likelihood.eval()
     with torch.no_grad():
-        predicts = likelihood(gpr(test_inputs))
+        predicts = run.likelihood(run.model(test_inputs))
         predicts_mean = predicts.mean
         predicts_std = predicts.stddev
 
