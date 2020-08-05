2ca41383af155302ee1d5c482740215cb0e29e14
Kaneta Yuya <yucho147@gmail.com>
2020-08-05 20:22:40 +0900

    Update README and sample data

---
+++ data/create_sample_data_2.py
@@ -0,0 +1,12 @@
+import numpy as np
+np.random.seed(14)
+
+num = 200
+sample_num = 7
+idx = np.random.randint(0, num, sample_num)
+
+x = np.linspace(0, 1, num)
+y = np.random.normal(np.sin(x * 20.) * 10., 3.0) + 10.
+
+data = np.array([x[idx], y[idx]]).T
+np.savetxt('./sample_data_2.csv', data, delimiter=',', header='x,y', comments='')
