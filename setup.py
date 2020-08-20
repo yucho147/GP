254b7fb164f5c67ac7f535edddcfa9ff847c713d
Kaneta Yuya <yucho147@gmail.com>
2020-08-20 20:00:46 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 import io
 import os
-import re
 
 from setuptools import find_packages, setup
 
@@ -13,8 +12,8 @@ def read(*names, **kwargs):
         return fp.read()
 
 
-torch_min = "1.5"
 install_requires = [
+    "pytorch",
     "gpytorch",
     "matplotlib",
     "numpy",
@@ -22,27 +21,17 @@ install_requires = [
     "PyYAML",
     "attrdict"
 ]
-try:
-    import torch
-
-    if torch.__version__ >= torch_min:
-        install_requires = []
-except ImportError:
-    pass
 
 
 # Run the setup
 setup(
     name="eagpytorch",
-    version='0.1.0',
+    version='0.1.1',
     description="We have created a module to run the Gaussian process model. We have implemented the code based on GPyTorch.",
-    # long_description=readme,  # .orgがpypiでは対応していない
-    # long_description_content_type="text/markdown",  # .orgがpypiでは対応していない
     author="Hirotaka Kato, Naofumi Emoto and Yuya Kaneta",
     url="https://github.com/yucho147/GP",
     author_email="yucho147@gmail.com",
     project_urls={
-        # "Documentation": "",
         "Source": "https://github.com/yucho147/GP",
     },
     license="MIT",
